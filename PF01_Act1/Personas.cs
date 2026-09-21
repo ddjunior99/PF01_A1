cb1f334b9bc2c2e7dc17723c0ef87376d502367f
Daniel Montes Cárdenas <danmoncar5@alu.edu.gva.es>
2026-09-21 19:48:24 +0200

    Creado constructor clase persona

--- PF01_Act1/Personas.cs
+++ PF01_Act1/Personas.cs
@@ -1,8 +1,38 @@
 ﻿using System;
 
-public class Class1
+public class Persona	
 {
-	public Class1()
+	public Persona()
 	{
-	}
+		//Propiedades
+    private string nombre;
+    private int edad;
+    private char sexo;
+
+    public string Nombre
+    {
+        get { return nombre; }
+        set { nombre = value; }
+    }
+
+    public int Edad
+    {
+        get { return edad; }
+        set { edad = value; }
+    }
+
+    public char Sexo
+    {
+        get { return sexo; }
+        set { sexo = value; }
+    }
+
+    //Constructor
+    Persona p = new Persona(string nombre, int edad, char sexo)
+    {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sexo = sexo;
+    }
+}
 }
