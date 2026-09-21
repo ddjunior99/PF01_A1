17559bcc4b05afc0ddac1c06750c31d192ad3f87
Daniel Montes Cárdenas <danmoncar5@alu.edu.gva.es>
2026-09-21 19:42:22 +0200

    Revert "Creada primera funcionalidad"
    This reverts commit d8b41cbd1444398dd5899f4b8894c21f69f2cc7b.

--- PF01_Act1/Program.cs
+++ PF01_Act1/Program.cs
@@ -1,12 +1,1 @@
-﻿using System.IO.Pipes;
-
-namespace PF01_Act1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            
-        }
-    }
-}
+﻿Console.WriteLine("Hello, World!");
