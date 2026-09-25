923ff780e9cbcc9663e215654cd392d3c4640333
Rodrigo Soler <rodsolvid@alu.edu.gva.es>
2026-09-25 19:01:48 +0200

    Creado metodo mayor o menor de edad

---
+++ PF01_Act1/Personas.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+public class Persona	
+{
+	public Persona()
+	{
+		//Propiedades
+    private string nombre;
+    private int edad;
+    private char sexo;
+
+    public string Nombre
+    {
+        get { return nombre; }
+        set { nombre = value; }
+    }
+
+    public int Edad
+    {
+        get { return edad; }
+        set { edad = value; }
+    }
+
+    public char Sexo
+    {
+        get { return sexo; }
+        set { sexo = value; }
+    }
+
+    //Constructor
+    Persona p = new Persona(string nombre, int edad, char sexo)
+    {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sexo = sexo;
+    }
+
+    //Métodos
+    public void MostrarInformacion()
+    {
+        Console.WriteLine("Nombre: " + nombre);
+        Console.WriteLine("Edad: " + edad);
+        Console.WriteLine("Sexo: " + sexo);
+    }
+
+    public void MayorEdad()
+	{
+		if (edad >= 18)
+		{
+			Console.WriteLine(nombre + " es mayor de edad.");
+		}
+		else
+		{
+			Console.WriteLine(nombre + " es menor de edad.");
+		}
+	}
+}
