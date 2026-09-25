44116bd7fbd9dc2bc7e7a112d1697931032480bc
Daniel Montes Cárdenas <danmoncar5@alu.edu.gva.es>
2026-09-25 19:14:37 +0200

    Llamada desde el main

--- PF01_Act1/Personas.cs
+++ PF01_Act1/Personas.cs
@@ -42,7 +42,7 @@ public class Persona
         Console.WriteLine("Edad: " + edad);
         Console.WriteLine("Sexo: " + sexo);
     }
-<<<<<<< HEAD
+
 
     public void MayorEdad()
 	{
@@ -55,11 +55,11 @@ public class Persona
 			Console.WriteLine(nombre + " es menor de edad.");
 		}
 	}
-=======
+
     public void CumplirAnios()
     {
         edad++;
         Console.WriteLine(nombre + " ha cumplido " + edad + " años.");
     }
->>>>>>> d6903d9fd351215dbd977746995cb7af4ed5d084
+
 }
--- PF01_Act1/Program.cs
+++ PF01_Act1/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace PF01_Act1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p = new Persona("Juan", 25, 'M');
+
+            p.MostrarInformacion();
+            p.CumplirAnios();
+        }
+    }
+}
